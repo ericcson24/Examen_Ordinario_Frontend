e69a916bef9bb6bfab6a06d373ed67943ad96e25
ericcson24 <150656730+ericcson24@users.noreply.github.com>
2025-06-17 20:10:39 +0200

    Add files via upload

---
+++ routes/index.tsx
@@ -0,0 +1,10 @@
+import Entry from "../islands/Entry.tsx";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Titulo página</h1>
+      <p>Subtitulo</p>
+    </div>
+  )
+}
---
+++ utils/type.ts
@@ -0,0 +1,2 @@
+import { ObjectId, OptionalId } from "mongodb";
+
